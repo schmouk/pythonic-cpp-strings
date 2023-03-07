b3d0cbc6541551be35e526c1dc392f6be6c841d9
Philippe Schmouker <ph.schmouker@yahoo.fr>
2023-03-07 14:29:42 +0100

    #2 - Create empty module cppstrings.h.
    Completed.

---
+++ cpp-strings/cppstrings.h
@@ -0,0 +1,23 @@
+#pragma once
+/**
+    Library cppstrings
+    "What if c++ strings where as easy to use as Python strings?"
+
+    Copyright (C) 2023 Philippe Schmouker
+    contact - ph (dot) schmouker (at) gmail (dot) com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation,  either version 3  of the License,  or
+    (at your option) any later version.
+
+    This program is distributed in the hope  that  it  will  be  useful,
+    but  WITHOUT  ANY  WARRANTY;  without  even  the implied warranty of
+    MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the
+    GNU General Public License for more details.
+
+    You should have received a copy  of  the  GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
