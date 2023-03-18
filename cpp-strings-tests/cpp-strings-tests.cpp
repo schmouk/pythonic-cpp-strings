11e0bb79c354fd7f6465bcfe32df0e24fb9fcb34
Philippe Schmouker <ph.schmouker@yahoo.fr>
2023-03-18 16:03:40 +0100

    #79 - Test pcs::is_ascii() for char and wchar_t
    Tested.

--- cpp-strings-tests/cpp-strings-tests.cpp
+++ cpp-strings-tests/cpp-strings-tests.cpp
@@ -10,7 +10,8 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace cppstringstests
 {
-	TEST_CLASS(cppstringstests)
+	//===   templated chars classes tests   ======================
+	TEST_CLASS(cppstringstests_PART_1)
 	{
 	public:
 		
@@ -20,6 +21,14 @@ namespace cppstringstests
 				Assert::AreEqual((const bool)std::isalpha(ch), pcs::is_alpha(char(ch)));
 			for (wchar_t ch=0; ch < L'\uffff'; ++ch)
 				Assert::AreEqual((const bool)std::iswalpha(ch), pcs::is_alpha(ch));
+		}
+
+		TEST_METHOD(is_ascii)
+		{
+			for (int ch = 0; ch <= 255; ++ch)
+				Assert::AreEqual(ch < 128, pcs::is_ascii(char(ch)));
+			for (wchar_t ch = 0; ch < L'\uffff'; ++ch)
+				Assert::AreEqual(int(ch) < 128, pcs::is_ascii(ch));
 		}
 	};
 }
